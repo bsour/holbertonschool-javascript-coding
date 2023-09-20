44711d4548d18b80d335697cf5845d508d687e89
Sourabh <sourabhbeniwal@outlook.com>
2023-09-20 14:12:34 +0000

    added task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+const fInput = process.argv[3];
+const fPath = process.argv[2];
+
+fs.writeFile(fPath, fInput, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
