7d3775a5e49cc0423c97531242b83c460fb5c43d
Sourabh <sourabhbeniwal@outlook.com>
2023-09-20 14:45:25 +0000

    added task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    if (response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      console.log(movieData.title);
+    } else {
+      console.error(`Error: Status code ${response.statusCode}`);
+    }
+  }
+});
