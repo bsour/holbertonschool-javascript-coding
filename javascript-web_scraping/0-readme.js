d2bf81098d14c5e89c883b8894a4e2594f75c600
Sourabh <sourabhbeniwal@outlook.com>
2023-09-20 14:00:51 +0000

    semi-standard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
-    } else {
-      console.log(data);
-    }
+  } else {
+    console.log(data);
+  }
 });
