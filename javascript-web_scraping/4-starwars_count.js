0767bc28f193f6b9c55e97f62ba1929563bd39cf
Sourabh <sourabhbeniwal@outlook.com>
2023-09-21 01:04:20 +0000

    added task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const characterId = 18;
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    if (response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      let movieCount = 0;
+      for (const movie of movieData.results) {
+        if (movie.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
+          movieCount++;
+        }
+      }
+
+      console.log(movieCount);
+    } else {
+      console.error(`Error: Status code ${response.statusCode}`);
+    }
+  }
+});
