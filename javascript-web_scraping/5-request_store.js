95159636d8c8e01a47156c9d750cedb92319a188
Sourabh <sourabhbeniwal@outlook.com>
2023-09-22 13:34:24 +0000

    added task 5 and 6

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,24 +1,19 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
-
+const request = require('request');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Send a request to the specified URL
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = body;
+    fs.writeFile(filePath, data, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
-
-  // Write the response body to the specified file (UTF-8 encoded)
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      process.exit(1);
-    }
-    console.log(`Contents from ${url} saved to ${filePath}`);
-  });
 });
--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const userData = JSON.parse(body);
+
+  const userCounts = userData.reduce((counts, user) => {
+    if (user.completed === true) {
+      counts[user.userId] = (counts[user.userId] || 0) + 1;
+    }
+    return counts;
+  }, {});
+
+  console.log(userCounts);
+});
