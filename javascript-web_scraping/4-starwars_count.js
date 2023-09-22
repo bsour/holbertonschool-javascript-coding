7b6a3315c4a88c62861a24551b346fc9d4f01ed0
Sourabh <sourabhbeniwal@outlook.com>
2023-09-22 13:18:48 +0000

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,36 +2,25 @@
 
 const request = require('request');
 
-const characterId = 18; // Assign the character ID
-
-const apiUrl = process.argv[2]; // Get the API URL from the command line argument
-
-if (!apiUrl) {
-  console.error('Usage: ./4-starwars_count.js <API_URL>');
+if (process.argv.length !== 3) {
+  console.error('Usage: ./4-starwars_count.js API_URL');
   process.exit(1);
 }
 
+const apiUrl = process.argv[2];
+const characterId = 18;
+
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      const movieData = JSON.parse(body);
-
-      // Initialize a count for movies where Wedge Antilles is present
-      let movieCount = 0;
-
-      // Loop through the movies and check if characterId (Wedge Antilles) is present
-      for (const movie of movieData.results) {
-        if (movie.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
-          movieCount++;
-        }
-      }
-
-      // Print the count of movies where Wedge Antilles is present
-      console.log(movieCount);
-    } else {
-      console.error(`Error: Status code ${response.statusCode}`);
-    }
+    console.error('Error:', error);
+    process.exit(1);
   }
+
+  const data = JSON.parse(body);
+
+  const filmsWithWedge = data.results.filter((film) =>
+    film.characters.includes(`${apiUrl}people/${characterId}/`)
+  );
+
+  console.log(filmsWithWedge.length);
 });
