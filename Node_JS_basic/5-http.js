d19a48918a4ece55f7a2c32bcf863e42211c1211
Sourabh <sourabhbeniwal@outlook.com>
2023-10-10 13:24:58 +1100

    updated task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,20 +2,24 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     countStudents(process.argv[2])
-      .then(() => {
+      .then((studentData) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('This is the list of our students\n');
+        res.write(studentData); // Assuming countStudents resolves with student data
         res.end();
       })
       .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' }); // Internal Server Error
         res.end(error.message);
       });
   } else {
-    res.end();
+    res.writeHead(404, { 'Content-Type': 'text/plain' }); // Not Found for other endpoints
+    res.end('Not Found');
   }
 });
 
