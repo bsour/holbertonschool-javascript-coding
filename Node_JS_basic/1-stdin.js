8ec4a1eee86b89e5ca160448c5931d74aeab6cc5
Sourabh <sourabhbeniwal@outlook.com>
2023-10-10 11:25:12 +1100

    changed \r to \n

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,12 +5,12 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?\r');
 
 rl.on('line', (name) => {
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\r`);
   if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
+    console.log('This important software is now closing\r');
     process.exit(0);
   } else {
     rl.close();
@@ -19,6 +19,6 @@ rl.on('line', (name) => {
 
 rl.on('close', () => {
   if (process.stdin.isTTY) {
-    console.log('This important software is now closing');
+    console.log('This important software is now closing\r');
   }
 });
