03f92956147de551cae9b323cc08c7fb53fa78bb
Sourabh <sourabhbeniwal@outlook.com>
2023-10-10 11:13:02 +1100

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+function promptUser() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+    console.log(`Your name is: ${name}`);
+    console.log('This important software is now closing');
+    rl.close();
+  });
+
+  rl.on('close', () => {
+    console.log('This important software is now closing');
+  });
+}
+
+module.exports = promptUser;
