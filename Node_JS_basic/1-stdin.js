e428896710329b4dba52c348b761e2aa9e5ce0e3
Sourabh <sourabhbeniwal@outlook.com>
2023-10-10 11:42:52 +1100

    passing all the test cases now

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,18 +1,14 @@
-// 1-stdin.js
+process.stdin.setEncoding('utf8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-    process.exit(0);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit(0);
 });
