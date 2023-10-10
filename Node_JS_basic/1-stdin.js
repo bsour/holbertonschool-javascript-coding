46922b1546167579a7a1d09f748830dd0171105d
Sourabh <sourabhbeniwal@outlook.com>
2023-10-10 11:23:26 +1100

    added process,stdin.isTTY

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,3 @@
-// const readline = require('readline');
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// console.log('Welcome to Holberton School, what is your name?');
-
-// rl.on('line', (name) => {
-//   console.log(`Your name is: ${name}`);
-//   rl.close();
-// });
-
-// rl.on('close', () => {
-//   console.log('This important software is now closing');
-// });
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -22,24 +5,20 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// Check if stdin is a TTY (Terminal Type), which means it's interactive
-if (process.stdin.isTTY) {
-  console.log('Welcome to Holberton School, what is your name?');
-} else {
-  // If not interactive, read data from the pipe
-  process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
-    process.exit();
-  });
-}
+console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (name) => {
   console.log(`Your name is: ${name}`);
-  rl.close();
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+    process.exit(0);
+  } else {
+    rl.close();
+  }
 });
 
 rl.on('close', () => {
-  console.log('This important software is now closing');
+  if (process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
 });
